020bb3330f77363a649a6a49bad1fbf825b45be4
Alan Vital <alanvital18@gmail.com>
2022-12-11 15:33:21 -0300

    chore: add testing library

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -2,8 +2,6 @@ import { Login } from '@/presentation/pages/login/login'
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import '@/presentation/styles/global.scss'
-
 export const Router: React.FC = () => {
   return (
     <BrowserRouter>
