eeee1af64956072a0526940020e7637cc0afbcac
Alan Vital <alanvital18@gmail.com>
2023-01-17 15:37:54 -0300

    feat: ensure RequiredFieldValidation returns falsy if field is not empty

---
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -0,0 +1,17 @@
+import { RequiredFieldError } from "@/validation/errors"
+import { faker } from "@faker-js/faker";
+import { RequiredFieldValidation } from "./required-field-validation";
+
+describe('RequiredFieldValidation', () => {
+  test('Should return error if field is empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate('')
+    expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
+  })
+})
