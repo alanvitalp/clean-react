83f4f6429658492ac6d0a6bed993c58ede073745
Alan Vital <alanvital18@gmail.com>
2023-01-27 09:53:55 -0300

    fix: change webpack env var to use JSON.stringify

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder } from '@/validation/validators/builder/validation-build'
-import { makeRemoteAuthentication } from '../../usecases/authentication/remote-authentication-factory'
 
 import { Login } from '@/presentation/pages/login/login'
+import { makeRemoteAuthentication } from '../../usecases/authentication/remote-authentication-factory'
 import { makeLoginValidation } from './login-validation-factory'
+import { makeLocalSaveAccessToken } from '../../usecases/save-access-token/local-save-access-token-factory'
 
 export const makeLogin: React.FC = () => {
   return (
-    <Login authentication={makeRemoteAuthentication()} validation={makeLoginValidation()} />
+    <Login authentication={makeRemoteAuthentication()} validation={makeLoginValidation()} saveAccessToken={makeLocalSaveAccessToken()}/>
   )
 }
