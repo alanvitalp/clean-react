530554b851cff38b164d53c93c4bc54bbefdcfdb
Alan Vital <alanvital18@gmail.com>
2023-02-10 08:43:10 -0300

    fix: change CompareFieldsValidation tes to never return same value

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -9,7 +9,10 @@ describe('RequiredFieldValidation', () => {
     const field = faker.random.word()
     const fieldToCompare = faker.random.word()
     const sut = makeSut(field, fieldToCompare)
-    const error = sut.validate({ [field]: faker.random.word(), [fieldToCompare]: faker.random.word() })
+    const error = sut.validate({
+      [field]: faker.random.words(3),
+      [fieldToCompare]: faker.random.words(4)
+    })
     expect(error).toEqual(new InvalidFieldError())
   })
 
