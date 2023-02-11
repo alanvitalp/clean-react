8f41a1a483cdfbb448ccb285b2719afba7c57085
Alan Vital <alanvital18@gmail.com>
2023-02-11 20:02:49 -0300

    feat: ensure SurveyItem load survey data correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -9,12 +9,13 @@ type Props = {
 }
 
 export const SurveyItem: React.FC<Props> = ({ survey }) => {
+  const iconName = survey.didAnswer ? IconName.THUMBS_UP : IconName.THUMBS_DOWN
   return (
     <li className={Styles.surveyItemWrap}>
       <div className={Styles.surveyContent}>
-        <Icon iconName={IconName.THUMBS_UP} />
+        <Icon iconName={iconName} />
         <time>
-          <span data-testid="day" className={Styles.day}>{survey.date.getDate()}</span>
+          <span data-testid="day" className={Styles.day}>{survey.date.getDate().toString().padStart(2, '0')}</span>
           <span data-testid="month" className={Styles.month}>{survey.date.toLocaleString('pt-BR', { month: 'short' }).replace('.', '')}</span>
           <span data-testid="year" className={Styles.year}>{survey.date.getFullYear()}</span>
         </time>
