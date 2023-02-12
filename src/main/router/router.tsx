ea22c88483ca2df4a7ac22e1db4b03807412f5cd
Alan Vital <alanvital18@gmail.com>
2023-02-12 10:46:44 -0300

    feat: make factory for SurveyList

--- src/main/router/router.tsx
+++ src/main/router/router.tsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignUp } from '@/main/factories/pages/signup/signup-factory'
-import { SurveyList } from '@/presentation/pages/survey-list/survey-list'
 import ApiContext from '@/presentation/contexts/api/api-context'
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '../adapters/current-account-adapter'
 import { PrivateRoute } from '@/presentation/components/private-route/private-route'
+import { makeSurveyList } from '../factories/pages/survey-list/survey-list-factory'
 
 export const Router: React.FC = () => {
   return (
@@ -18,7 +18,7 @@ export const Router: React.FC = () => {
           <Switch>
             <Route path="/login" exact component={makeLogin} />
             <Route path="/signup" exact component={makeSignUp} />
-            <PrivateRoute path="/" exact component={SurveyList} />
+            <PrivateRoute path="/" exact component={makeSurveyList} />
           </Switch>
       </BrowserRouter>
     </ApiContext.Provider>
