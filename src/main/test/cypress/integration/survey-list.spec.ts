00983aa3a91be83a0b95abc8621bb24cffb3951d
Alan Vital <alanvital18@gmail.com>
2023-02-18 18:38:48 -0300

    test: ensure SurveyList reaload on button click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -11,6 +11,10 @@ export const mockAccessDeniedError = (): void => {
   Http.mockForbiddenError('GET', path)
 }
 
+export const mockSuccess = (): void => {
+  Http.mockOk('GET', path, 'fx:survey-list')
+}
+
 describe('Login', () => {
   beforeEach(() => {
     cy.fixture('account').then(account => {
@@ -22,6 +26,15 @@ describe('Login', () => {
     mockUnexpectedError()
     cy.visit('')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+
+  it('Should reaload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+    mockSuccess()
+    cy.getByTestId('reload').click()
+    cy.get('li:empty').should('have.length', 4)
   })
 
   it('Should logout on AccessDeniedError', () => {
@@ -42,5 +55,27 @@ describe('Login', () => {
     cy.visit('')
     cy.getByTestId('logout').click()
     Helper.testUrl('/login')
+  })
+
+  it('Should present surveys items', () => {
+    mockSuccess()
+    cy.visit('')
+    cy.get('li:empty').should('have.length', 4)
+    cy.get('li:not(:empty)').should('have.length', 2)
+    cy.get('li:nth-child(1)').then(li => {
+      assert.equal(li.find('[data-testid="day"]').text(), '03')
+      assert.equal(li.find('[data-testid="month"]').text(), 'fev')
+      assert.equal(li.find('[data-testid="year"]').text(), '2018')
+      assert.equal(li.find('[data-testid="question"]').text(), 'Question 1')
+    })
+    cy.get('li:nth-child(1) [data-testid="thumbs-up"]').should('exist')
+
+    cy.get('li:nth-child(2)').then(li => {
+      assert.equal(li.find('[data-testid="day"]').text(), '04')
+      assert.equal(li.find('[data-testid="month"]').text(), 'fev')
+      assert.equal(li.find('[data-testid="year"]').text(), '2018')
+      assert.equal(li.find('[data-testid="question"]').text(), 'Question 2')
+    })
+    cy.get('li:nth-child(2) [data-testid="thumbs-down"]').should('exist')
   })
 })
