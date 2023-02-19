913cef999ae059581e4b962d96af04223420cf7f
Alan Vital <alanvital18@gmail.com>
2023-02-19 12:24:15 -0300

    feat: ensure RemoteLoadSurveyResult throws AccessDeniedError on 403

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,4 +1,6 @@
+import { HttpStatusCode } from "@/data/protocols/http"
 import { HttpGetClientSpy } from "@/data/test"
+import { AccessDeniedError } from "@/domain/errors"
 import { faker } from "@faker-js/faker"
 import { RemoteLoadSurveyResult } from "./remote-load-survey-result"
 
@@ -22,5 +24,14 @@ describe('RemoteLoadSurveyResult', () => {
     const { sut, httpGetClientSpy } = makeSut(url)
     await sut.load()
     expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  it('Should throw AccessDeniedError if HttpGetClient returns 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
