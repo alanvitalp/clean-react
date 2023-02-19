3e6c962abeb5f88c90f6cc3f04c7826d5ec8553f
Alan Vital <alanvital18@gmail.com>
2023-02-19 12:18:27 -0300

    feat: RemoteLoadSurveyResult call HttpGetClient with correct values

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -0,0 +1,26 @@
+import { HttpGetClientSpy } from "@/data/test"
+import { faker } from "@faker-js/faker"
+import { RemoteLoadSurveyResult } from "./remote-load-survey-result"
+
+type SutTypes = {
+  sut: RemoteLoadSurveyResult
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
+describe('RemoteLoadSurveyResult', () => {
+  it('Should call HttpGetClient with correct values', async () => {
+    const url = faker.internet.url()
+    const { sut, httpGetClientSpy } = makeSut(url)
+    await sut.load()
+    expect(httpGetClientSpy.url).toBe(url)
+  })
+})
