498b36f7fa6858bde52e5971b21e83c9ce969666
Alan Vital <alanvital18@gmail.com>
2023-02-27 22:30:31 -0300

    feat: ensure RemoteSaveSurveyResult calls httpClient with correct values

--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -2,3 +2,5 @@ export { RemoteLoadSurveyResult } from './load-survey-result/remote-load-survey-
 export * from './load-survey-list/remote-load-survey-list'
 export * from './add-account/remote-add-account'
 export * from './authentication/remote-authentication'
+export * from './save-survey-result/remote-save-survey-result'
+
---
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -0,0 +1,28 @@
+import { RemoteSurveyResultModel } from "@/data/models";
+import { HttpClient, HttpStatusCode } from "@/data/protocols/http";
+import { AccessDeniedError, UnexpectedError } from "@/domain/errors";
+import { LoadSurveyResult } from "@/domain/usecases";
+import { SaveSurveyResult } from "@/domain/usecases/save-survey-result";
+
+export class RemoteSaveSurveyResult implements SaveSurveyResult {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpClient<RemoteSaveSurveyResult.Model>
+  ) {}
+
+  async save (params: SaveSurveyResult.Params): Promise<SaveSurveyResult.Model> {
+    const httpResponse = await this.httpClient.request({ url: this.url, method: 'put', body: params })
+    const remoteSurveyResult = httpResponse.body
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: return Object.assign({}, remoteSurveyResult, {
+        date: new Date(remoteSurveyResult.date)
+      })
+      case HttpStatusCode.forbidden: throw new AccessDeniedError()
+      default: throw new UnexpectedError()
+    }
+  }
+}
+
+export namespace RemoteSaveSurveyResult {
+  export type Model = RemoteSurveyResultModel
+}
